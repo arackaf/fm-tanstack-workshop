13ee06f80cd478d3b80378c11e58b366558fbb87
Adam Rackis <adamrackis@hotmail.com>
2026-02-17 23:49:44 -0600

    add relations to db.ts

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 import * as schema from "./schema";
+import * as relations from "./relations";
 
 const TARGET_DB_NAME = "tanstack-jacked";
 
@@ -14,4 +15,9 @@ if (!postgresUrl) {
 const connectionString = `${postgresUrl}/${TARGET_DB_NAME}`;
 const pool = new Pool({ connectionString });
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(pool, {
+  schema: {
+    ...schema,
+    ...relations,
+  },
+});
