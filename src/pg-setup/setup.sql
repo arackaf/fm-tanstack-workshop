f31473b9d2025d4eb935a8b4a66111a467e061af
Adam Rackis <adamrackis@hotmail.com>
2026-02-17 22:38:01 -0600

    sql setup work

--- src/pg-setup/setup.sql
+++ src/pg-setup/setup.sql
@@ -8,10 +8,33 @@ CREATE TABLE IF NOT EXISTS users (
   UNIQUE (username)
 );
 
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'muscle_group') THEN
+    CREATE TYPE muscle_group AS ENUM (
+      'chest',
+      'shoulders',
+      'biceps',
+      'triceps',
+      'quadriceps',
+      'hamstrings',
+      'calves',
+      'lats',
+      'back'
+    );
+  END IF;
+END
+$$;
+
+
 CREATE TABLE IF NOT EXISTS exercises (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(150),
   description TEXT,
-  muscle_group TEXT,
+  muscle_groups muscle_group[],
   is_compound BOOL
 );
+
+CREATE INDEX IF NOT EXISTS idx_exercises_muscle_groups_gin
+  ON exercises
+  USING GIN (muscle_groups);
