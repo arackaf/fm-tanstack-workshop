9b85ea7f5edf6b2e81b531d583f8a7b331de0efe
Adam Rackis <adamrackis@hotmail.com>
2026-02-17 23:34:06 -0600

    fix dotenv

--- src/pg-setup/run-setup.ts
+++ src/pg-setup/run-setup.ts
@@ -1,7 +1,11 @@
 import dotenv from "dotenv";
 
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { setupIfNeeded } from "./setup";
 
-dotenv.config({ path: "../../.env" });
+const thisFileDir = dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: join(thisFileDir, "../../.env") });
 
 setupIfNeeded();
