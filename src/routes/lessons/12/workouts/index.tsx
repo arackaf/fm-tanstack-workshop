6e4e44210516a6834a77ad108a7eff14fa0da778
Adam Rackis <adamrackis@hotmail.com>
2026-03-27 20:03:54 -0500

    Set up lesson 12

--- src/routes/lessons/12/workouts/index.tsx
+++ src/routes/lessons/12/workouts/index.tsx
@@ -10,67 +10,15 @@ export const Route = createFileRoute("/lessons/12/workouts/")({
   component: RouteComponent,
 });
 
-const __prelim_middleware1 = createMiddleware({ type: "function" })
-  .inputValidator((input: { middlewareArg: string }) => input)
-  .client(async ({ data, next, context }) => {
-    const { middlewareArg } = data;
-
-    console.log("In middleware on Client, before", middlewareArg);
-
-    return next({
-      sendContext: {
-        sendContextSetInClient: "x",
-      },
-      context: {
-        contextSetInClient: "y",
-      },
-    });
-  })
-  .server(async ({ next, data, context }) => {
-    const { middlewareArg } = data;
-    const { sendContextSetInClient } = context;
-
-    console.log("In middleware on Server, before", {
-      middlewareArg,
-      sendContextSetInClient,
-    });
-
-    const result = await next({
-      context: {
-        contextSetInServerInMiddleware: "aaaa",
-      },
-      sendContext: {
-        valueSentFromServerToClient: "Yoooooo",
-      },
-    });
-
-    console.log("In middleware on Server, after", middlewareArg);
-
-    return result;
-  });
-
-const middleware1 = createMiddleware({ type: "function" })
-  .middleware([__prelim_middleware1])
-  .client(async ({ next, context }) => {
-    const result = await next();
-
-    const xxx = result.context.valueSentFromServerToClient;
-    console.log("Here's the vlaue the right way", xxx);
-
-    return result;
-  });
-
 const serverFn = createServerFn({ method: "GET" })
   .inputValidator((input: { functionArg: string }) => input)
-  .middleware([middleware1])
-  .handler(async ({ data, context }) => {
-    context.contextSetInServerInMiddleware;
+  .handler(async ({}) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
+
     console.log(
       "\n",
       "==============================================\n",
       "I'm in a server function!!!\n",
-      `Server Function Arg: ${data.functionArg}\n`,
       "==============================================\n",
       "\n",
     );
@@ -89,7 +37,6 @@ function RouteComponent() {
         onClick={() =>
           call({
             data: {
-              middlewareArg: "Middleware Arg",
               functionArg: "Function Arg",
             },
           })
